9ab2b0a73c386c61c68d17ca6241ca9507b94d3c
Jan Reitz <jan-reitz@gmx.de>
2021-01-10 12:53:57 +0100

    Add custom shader for transparent material from bevy example

--- src/random_moving_balls.rs
+++ src/random_moving_balls.rs
@@ -1,11 +1,23 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    reflect::TypeUuid,
+    render::{
+        mesh::shape,
+        pipeline::{PipelineDescriptor, RenderPipeline},
+        render_graph::{base, AssetRenderResourcesNode, RenderGraph},
+        renderer::RenderResources,
+        shader::ShaderStages,
+    },
+};
 use rand::random;
 use crate::bvh::{BVHNode, AABB};
 
 pub struct RandomMovingBallsPlugin;
 impl Plugin for RandomMovingBallsPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_startup_system(spawn_balls.system())
+        app.add_asset::<MyMaterial>()
+            .add_startup_system(setup.system())
+            .add_startup_system(spawn_balls.system())
             .add_system(move_balls.system())
             .add_system(test_color_balls_bvs.system());
     }
@@ -141,4 +153,65 @@ fn spawn_ball(
             ..Default::default()
         })
         .with(RandomMovingBall);
+}
+
+
+// Transparent Material
+#[derive(RenderResources, Default, TypeUuid)]
+#[uuid = "3bf9e364-f29d-4d6c-92cf-93298466c620"]
+struct MyMaterial {
+    pub color: Color,
+}
+
+fn setup(
+    commands: &mut Commands,
+    asset_server: ResMut<AssetServer>,
+    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<MyMaterial>>,
+    mut render_graph: ResMut<RenderGraph>,
+) {
+    // Watch for changes
+    asset_server.watch_for_changes().unwrap();
+
+    // Create a new shader pipeline with shaders loaded from the asset directory
+    let pipeline_handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
+        vertex: asset_server.load::<Shader, _>("shaders/hot.vert"),
+        fragment: Some(asset_server.load::<Shader, _>("shaders/hot.frag")),
+    }));
+
+    // Add an AssetRenderResourcesNode to our Render Graph. This will bind MyMaterial resources to our shader
+    render_graph.add_system_node(
+        "my_material",
+        AssetRenderResourcesNode::<MyMaterial>::new(true),
+    );
+
+    // Add a Render Graph edge connecting our new "my_material" node to the main pass node. This ensures "my_material" runs before the main pass
+    render_graph
+        .add_node_edge("my_material", base::node::MAIN_PASS)
+        .unwrap();
+
+    // Create a new material
+    let material = materials.add(MyMaterial {
+        color: Color::rgb(0.0, 0.8, 0.0),
+    });
+
+    // Setup our world
+    commands
+        // cube
+        .spawn(MeshBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 2.0 })),
+            render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::new(
+                pipeline_handle,
+            )]),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+            ..Default::default()
+        })
+        .with(material)
+        // camera
+        .spawn(Camera3dBundle {
+            transform: Transform::from_translation(Vec3::new(3.0, 5.0, -8.0))
+                .looking_at(Vec3::default(), Vec3::unit_y()),
+            ..Default::default()
+        });
 }
