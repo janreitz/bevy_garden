f5c23c5ec9cfee7fc543132606629b8086565ef2
Jan Reitz <jan-reitz@gmx.de>
2021-01-13 23:11:05 +0100

    add custom bird resource

--- src/boids.rs
+++ src/boids.rs
@@ -305,26 +305,29 @@ fn update_boids(
 
 fn spawn_boids(
         commands: &mut Commands,
-        mut meshes: ResMut<Assets<Mesh>>,
+        mut _meshes: ResMut<Assets<Mesh>>,
         mut materials: ResMut<Assets<StandardMaterial>>,
-        _asset_server: ResMut<AssetServer>,
+        asset_server: ResMut<AssetServer>,
 ) {
     let num_boids = 80;
 
-    let mesh_handle_box = meshes.add(
-        Mesh::from(
-            shape::Box::new(1.0, 1.0, 1.0) 
-        )
-    );
+    // let mesh_handle_box = meshes.add(
+    //     Mesh::from(
+    //         shape::Box::new(1.0, 1.0, 1.0) 
+    //     )
+    // );
+
+    let mesh_handle: Handle<Mesh> = asset_server.load("models/birds/bird.glb#Mesh0/Primitive0");
+
     let material_handle = materials.add(Color::rgb(1.0, 0.9, 0.9).into());
  
-
     // Spawn random boids
     for _ in 0..num_boids {
         let mut transform = Transform::from_translation(random_vec3() * 8.0);
+        transform.apply_non_uniform_scale(Vec3::splat(0.1));
         transform.apply_non_uniform_scale(Vec3::splat(0.25));
         commands.spawn(PbrBundle {
-                mesh: mesh_handle_box.clone(),
+                mesh: mesh_handle.clone(),
                 material: material_handle.clone(),
                 transform: transform,
                 ..Default::default()
